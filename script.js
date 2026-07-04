ff849258a116f87cc16cf7342d95f10f4b766bdc
prathiksha7532 <prathiksha7532@gmail.com>
2026-07-04 10:03:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const registerBtn = document.querySelectorAll("button")[0];
+    const attendanceBtn = document.querySelectorAll("button")[1];
+    const viewBtn = document.querySelectorAll("button")[2];
+
+    registerBtn.onclick = function () {
+        alert("Student Registration Page (Coming Soon)");
+    };
+
+    attendanceBtn.onclick = function () {
+        alert("Face Recognition Attendance (Coming Soon)");
+    };
+
+    viewBtn.onclick = function () {
+        alert("Attendance Records (Coming Soon)");
+    };
+
+});
